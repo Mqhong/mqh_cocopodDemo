f5d843a3b1c873e68c47c751e76e66ac6fc1849a
孟庆洪 <qinghong.meng@socialcredits.cn>
2019-10-12 16:12:41 +0800

    0.0.2

---
+++ SourceFiles/Tool_Class.swift
@@ -0,0 +1,17 @@
+//
+//  Tool_Class.swift
+//  swift_ddd
+//
+//  Created by 孟庆洪 on 2019/10/8.
+//  Copyright © 2019 mm. All rights reserved.
+//
+
+import UIKit
+
+func RGBCOLOR(r:CGFloat,g g:CGFloat,b b:CGFloat) -> UIColor {
+    return UIColor(red: (r)/255.0, green: (g)/255.0, blue: (b)/255.0, alpha: 1.0)
+}
+
+class Tool_Class: NSObject {
+
+}
